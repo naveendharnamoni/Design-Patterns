0684aada2fa5237a618161f97f359612cc25f6d2
Naveen Dharnamoni <Naveen.d@Technovert.com>
2022-03-05 11:20:37 +0530

    implemented strategy pattern

---
+++ StrategyPattern/Business/Strategies/SalesTax/UsSalesTaxStrategy.cs
@@ -0,0 +1,21 @@
+﻿using StrategyPattern.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern.Business.Strategies.SalesTax
+{
+    public class UsSalesTaxStrategy : ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order)
+        {
+            switch (order.ShippingDetails.DestinationState.ToLowerInvariant())
+            {
+                case "la": return order.TotalPrice * 0.095m;
+                case "ny": return order.TotalPrice * 0.04m;
+                case "nyc": return order.TotalPrice * 0.045m;
+                default: return 0m;
+            }
+        }
+    }
+}
--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using StrategyPattern.Business.Models;
+using StrategyPattern.Business.Strategies.SalesTax;
 using System;
 
 namespace StrategyPattern
@@ -15,7 +16,15 @@ namespace StrategyPattern
                     DestinationCountry = "Sweden"
                 }
             };
-            
+            var destination = order.ShippingDetails.DestinationCountry.ToLowerInvariant();
+            if (destination == "sweden")
+            {
+                order.SalesTaxStrategy = new SwedenSalesTaxStrategy();
+            }
+            else if (destination == "us")
+            {
+                order.SalesTaxStrategy = new UsSalesTaxStrategy();
+            }
             order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
             order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m, ItemType.Service), 1);
 
