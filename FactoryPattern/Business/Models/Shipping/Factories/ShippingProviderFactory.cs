170537a0379fc95bc321368e8a4d4c9403205656
Naveen Dharnamoni <Naveen.d@Technovert.com>
2022-03-18 11:28:26 +0530

    Created factories for different type of shipping providers

---
+++ FactoryPattern/Business/Models/Shipping/Factories/ShippingProviderFactory.cs
@@ -0,0 +1,95 @@
+﻿using FactoryPattern.Business.Models.Commerce;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern.Business.Models.Shipping.Factories
+{
+    public abstract class ShippingProviderFactory
+    {
+        public abstract ShippingProvider CreateShippingProvider(string country);
+
+        public ShippingProvider GetShippingProvider(string country)
+        {
+            var provider = CreateShippingProvider(country);
+            return provider;
+        }
+
+    }
+
+    public class StandardShippingProviderFactory : ShippingProviderFactory
+    {
+        public override ShippingProvider CreateShippingProvider(string country)
+        {
+            #region Create Shipping Provider
+            ShippingProvider shippingProvider;
+
+            if (country == "Australia")
+            {
+                #region Australia Post Shipping Provider
+                var shippingCostCalculator = new ShippingCostCalculator(
+                    internationalShippingFee: 250,
+                    extraWeightFee: 500
+                )
+                {
+                    ShippingType = ShippingType.Standard
+                };
+
+                var customsHandlingOptions = new CustomsHandlingOptions
+                {
+                    TaxOptions = TaxOptions.PrePaid
+                };
+
+                var insuranceOptions = new InsuranceOptions
+                {
+                    ProviderHasInsurance = false,
+                    ProviderHasExtendedInsurance = false,
+                    ProviderRequiresReturnOnDamange = false
+                };
+
+                shippingProvider = new AustraliaPostShippingProvider("CLIENT_ID",
+                    "SECRET",
+                    shippingCostCalculator,
+                    customsHandlingOptions,
+                    insuranceOptions);
+                #endregion
+            }
+            else if (country == "Sweden")
+            {
+                #region Swedish Postal Service Shipping Provider
+                var shippingCostCalculator = new ShippingCostCalculator(
+                    internationalShippingFee: 50,
+                    extraWeightFee: 100
+                )
+                {
+                    ShippingType = ShippingType.Express
+                };
+
+                var customsHandlingOptions = new CustomsHandlingOptions
+                {
+                    TaxOptions = TaxOptions.PayOnArrival
+                };
+
+                var insuranceOptions = new InsuranceOptions
+                {
+                    ProviderHasInsurance = true,
+                    ProviderHasExtendedInsurance = false,
+                    ProviderRequiresReturnOnDamange = false
+                };
+
+                shippingProvider = new SwedishPostalServiceShippingProvider("API_KEY",
+                    shippingCostCalculator,
+                    customsHandlingOptions,
+                    insuranceOptions);
+                #endregion
+            }
+            else
+            {
+                throw new NotSupportedException("No shipping provider found for origin country");
+            }
+            return shippingProvider;
+            #endregion
+        }
+
+    }
+}
