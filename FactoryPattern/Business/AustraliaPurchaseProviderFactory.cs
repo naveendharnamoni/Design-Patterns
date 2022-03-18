1a2ff9581291804629f66b81516543c9fefcfab3
Naveen Dharnamoni <Naveen.d@Technovert.com>
2022-03-18 18:01:34 +0530

    added factories for different countries

---
+++ FactoryPattern/Business/AustraliaPurchaseProviderFactory.cs
@@ -0,0 +1,28 @@
+﻿using FactoryPattern.Business.Models.Commerce;
+using FactoryPattern.Business.Models.Commerce.Invoice;
+using FactoryPattern.Business.Models.Commerce.Summary;
+using FactoryPattern.Business.Models.Shipping;
+using FactoryPattern.Business.Models.Shipping.Factories;
+
+namespace FactoryPattern.Business
+{
+    public class AustraliaPurchaseProviderFactory : IPurchaseProviderFactory
+    {
+        public IInvoice CreateInvoice(Order order)
+        {
+            return new GSTInvoice();
+        }
+
+        public ShippingProvider CreateShippingProvider(Order order)
+        {
+            var shippingProviderFactory = new StandardShippingProviderFactory();
+
+            return shippingProviderFactory.GetShippingProvider(order.Sender.Country);
+        }
+
+        public ISummary CreateSummary(Order order)
+        {
+            return new CsvSummary();
+        }
+    }
+}
---
+++ FactoryPattern/Business/SwedenPurchaseProviderFactory.cs
@@ -0,0 +1,43 @@
+﻿using FactoryPattern.Business.Models.Commerce;
+using FactoryPattern.Business.Models.Commerce.Invoice;
+using FactoryPattern.Business.Models.Commerce.Summary;
+using FactoryPattern.Business.Models.Shipping;
+using FactoryPattern.Business.Models.Shipping.Factories;
+
+namespace FactoryPattern.Business
+{
+    public class SwedenPurchaseProviderFactory : IPurchaseProviderFactory
+    {
+        public IInvoice CreateInvoice(Order order)
+        {
+            if(order.Recipient.Country != order.Sender.Country)
+            {
+                return new NoVATInvoice();
+            }
+
+            return new VATInvoice();    
+        }
+
+        public ShippingProvider CreateShippingProvider(Order order)
+        {
+            ShippingProviderFactory shippingProviderFactory;
+
+            if(order.Sender.Country != order.Recipient.Country)
+            {
+                shippingProviderFactory = new GlobalExpressShippingProviderFactory();
+            }
+            else
+            {
+                shippingProviderFactory = new StandardShippingProviderFactory();
+            }
+
+            return shippingProviderFactory.GetShippingProvider(order.Sender.Country);
+        }
+
+        public ISummary CreateSummary(Order order)
+        {
+            // Translate email to Swedish
+            return new EmailSummary();
+        }
+    }
+}
