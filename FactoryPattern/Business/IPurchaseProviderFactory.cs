74242ecdfddcd8c6946d826c7a1f2702d44bc5f3
Naveen Dharnamoni <Naveen.d@Technovert.com>
2022-03-18 17:51:23 +0530

    implement purchase provider factories for each country

---
+++ FactoryPattern/Business/IPurchaseProviderFactory.cs
@@ -0,0 +1,14 @@
+﻿using FactoryPattern.Business.Models.Commerce;
+using FactoryPattern.Business.Models.Commerce.Invoice;
+using FactoryPattern.Business.Models.Commerce.Summary;
+using FactoryPattern.Business.Models.Shipping;
+
+namespace FactoryPattern.Business
+{
+    public interface IPurchaseProviderFactory
+    {
+        ShippingProvider CreateShippingProvider(Order order);
+        IInvoice CreateInvoice(Order order);
+        ISummary CreateSummary(Order order);
+    }
+}
