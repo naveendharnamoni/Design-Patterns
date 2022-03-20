333a4517c0de8691453564bc81cacafc66ee7dfd
Naveen Dharnamoni <Naveen.d@Technovert.com>
2022-03-20 07:07:09 +0530

    implemented lazy sngleton

---
+++ Singleton/Lazy/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Lazy
+{
+    public sealed class Singleton
+    {
+        private static Lazy<Singleton> _lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _lazy.Value;
+            }
+        }
+
+        private Singleton()
+        {
+
+        }
+    }
+}
