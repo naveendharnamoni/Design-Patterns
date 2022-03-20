c8b796bb639edbea02df4d95f0261fd2e0f47eb8
Naveen Dharnamoni <Naveen.d@Technovert.com>
2022-03-20 09:00:52 +0530

    Implemented mediator pattern

---
+++ Mediator/Program.cs
@@ -0,0 +1,20 @@
+﻿using Mediator.Structural;
+using System;
+
+namespace Mediator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mediator = new ConcreteMediator();
+            var c1 = new Colleague1(mediator);
+            var c2 = new Colleague2(mediator);
+            mediator.Colleague1 = c1;
+            mediator.Colleague2 = c2;
+
+            c1.Send("hello from c1");
+            c2.Send("hi from c2");
+        }
+    }
+}
---
+++ Mediator/Structural/Colleague.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.Structural
+{
+    public abstract class Colleague
+    {
+        protected Mediator Mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.Mediator = mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            this.Mediator.Send(message, this);
+        }
+
+        public abstract void HandleNotification(string message);
+    }
+}
